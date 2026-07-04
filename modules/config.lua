ac8801beba60d34bef6a5327f4f3df69ae945f0a
alikhandwawd <108534379+alikhandwawd@users.noreply.github.com>
2026-07-04 17:14:07 +0500

    Update print statement from 'Hello' to 'Goodbye'

--- modules/config.lua
+++ modules/config.lua
@@ -2,11 +2,10 @@ local function asyncHttpRequest(method, url, args, resolve, reject)
 	local request_thread = effil.thread(function(method, url, args)
 		local requests = require 'requests'
 		local result, response = pcall(requests.request, method, url, args)
-		if result then
-			response.json, response.xml = nil, nil
-			return true, response
+		if result and response then
+			return true, tostring(response.status_code or 0), tostring(response.text or "")
 		else
-			return false, response
+			return false, tostring(response)
 		end
 	end)(method, url, args)
 	if not resolve then resolve = function() end end
@@ -17,8 +16,12 @@ local function asyncHttpRequest(method, url, args, resolve, reject)
 			local status, err = runner:status()
 			if not err then
 				if status == 'completed' then
-					local result, response = runner:get()
-					if result then resolve(response) else reject(response) end
+					local result, statusCodeOrErr, text = runner:get()
+					if result then
+						resolve({status_code = tonumber(statusCodeOrErr) or 0, text = text})
+					else
+						reject(statusCodeOrErr)
+					end
 					return
 				elseif status == 'cancelled' then
 					return reject(status)
